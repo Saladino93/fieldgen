b4c3ac6609960e4bc3f4540a5482f34707f50d70
Saladino93 <saladino_93@hotmail.it>
2023-09-04 16:22:34 +0200

    sampling poisson simple

--- fieldgen/catutils.py
+++ fieldgen/catutils.py
@@ -6,6 +6,8 @@ import astropy.io as astro_io
 
 import numba as nb
 
+import healpy as hp
+
 
 def read_catalog(filename: str):
     hdul = astro_io.fits.open(name = filename)
@@ -96,7 +98,34 @@ def poisson_sampling(counts_map: int, seed: int) -> np.ndarray:
     return rng.poisson(lam = counts_map)
 
 
-def delta_g_to_number_counts(delta_g_input_real: np.ndarray, No_observed: int, seed: int, mask: np.ndarray = 1, weights: np.ndarray = 1, alpha: float = 1.) -> np.ndarray:
+def delta_g_to_number_counts(delta_g_input_real: np.ndarray, No_observed: int, seed: int, mask: np.ndarray = 1, weights: np.ndarray = 1, alpha: float = 1., set_zero: bool = False) -> np.ndarray:
+
+    '''
+        This function transforms a delta_g map to a number counts map. For alpha < 1 see https://arxiv.org/pdf/1708.01536.pdf
+
+        Parameters
+        ----------
+        No_observed : int
+            Number of observed galaxies.
+        delta_g_input_real : np.ndarray
+            Delta_g map.
+        seed : int
+            Seed for the random number generator.
+        mask : np.ndarray
+            Mask of the map. e.g. completeness mask.
+        weights : np.ndarray
+            Weights of the map. This is useful if you want to contaminate the map with systematics.
+        alpha : float
+            Scaling factor for the delta_g map.
+        set_zero: bool
+            If True, sets to zero negative values of numbers.
+
+        Returns
+        -------
+        np.ndarray
+            Number counts map.
+        '''
+    
     
     #this is here so that if I do not want to sample, e.g. because I have a kappa map, I can just return the input map
     if No_observed == 0:
@@ -108,5 +137,46 @@ def delta_g_to_number_counts(delta_g_input_real: np.ndarray, No_observed: int, s
 
     Ngal = mappa*No_observed/Npix
     Ngal = np.nan_to_num(Ngal)*mask*np.nan_to_num(1/weights)/alpha**2.
+
+    if (alpha == 1) and set_zero:
+            print("alpha == 1, might set to 0 negative values")
+            Ngal[np.where(Ngal < 0.)] = 0. #setting to -1 the delta_g_input_real
         
     return (poisson_sampling(Ngal, seed)).astype(int)
+
+
+def get_catalog(seed: int, Npix: int, nside: int, number_counts: np.ndarray, z: np.ndarray, nz: np.ndarray, sample_z: bool = False):
+
+    ipix = np.arange(0, Npix)
+    ras, decs = hp.pixelfunc.pix2ang(nside, ipix, lonlat = True)
+    coords = np.vstack((ras, decs))
+
+    if type(number_counts) is not list:
+        number_counts = [number_counts]
+        z = [z]
+        nz = [nz]
+
+    #sample angular coordinates
+    catalog = np.hstack([np.repeat(coords, number_counts_.astype(int), axis = -1) for number_counts_ in number_counts])
+
+
+    if sample_z:
+        zs = np.hstack([sample_from_nz_pdf(z[ii], nz[ii], np.sum(number_counts_), seed = seed) for ii, number_counts_ in enumerate(number_counts)])
+    else:
+        zs = np.hstack([np.repeat(ii, np.sum(number_counts_)) for ii, number_counts_ in enumerate(number_counts)])
+
+    ras = catalog[0, :]
+    decs = catalog[1, :]
+
+    del catalog
+
+    weights_maps = [np.ones(Npix) for _ in range(len(number_counts))]
+
+    fake_weights = np.concatenate([np.repeat(weights, number_counts_.astype(int), axis = -1) for weights, number_counts_ in zip(weights_maps, number_counts)])
+
+    weights = fake_weights
+
+    #just a label to identify each redshift bin
+    zbin = np.concatenate([i*np.ones(np.sum(number_counts_)) for i, number_counts_ in enumerate(number_counts)])
+
+    return (ras, decs, zs, zbin, weights)
